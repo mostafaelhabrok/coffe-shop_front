e5186d932f4f981f5a3ed4498ac959f311070f8a
mostafaelhabrok <mostafa.elhabrok@gmail.com>
2021-07-19 15:48:22 +0200

    3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiServerUrl: 'https://coffe-shop-back-1.herokuapp.com', // the running FLASK api server url
+  auth0: {
+    url: 'mostafaelhabrok.us', // the auth0 domain prefix
+    audience: 'coffee-shop', // the audience set for the auth0 app
+    clientId: '8WqL4GWUB0mENmoqR3tYqGANArfmt3Z2', // the client id generated for the auth0 app
+    callbackURL: 'https://coffe-shop-front-1.herokuapp.com', // the base url of the running ionic application. 
+  }
 };
